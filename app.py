6fe4178938dde1b9746781ee858c9e7e9618accf
Cameron <buschcameron1@gmail.com>
2025-01-03 12:53:48 -0600

    finalized slop

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, render_template, request
+from flask import Flask, render_template, request
 import json, requests
 from markdown import markdown
 
@@ -6,29 +6,29 @@ app = Flask(__name__)
 
 @app.route("/", methods = ["GET", "POST"])
 def main():
-    body = {
-        "model": "llama3.2",
-        "prompt": "List 4 randomly selected words comma seperated. Only return the 4 words no other content",
-        "stream": False
-    }
-
-    links = requests.post("http://192.168.1.100:11434/api/generate", json=body)
+    links = requests.get("https://random-word-api.herokuapp.com/word?number=4")
     links = json.loads(links.content)
-    links = links['response'].split(',')
-    links = [x.strip(' ') for x in links]
         
     if not request.args.get('article'):
-        title = "Welcome to the home page!"
-        article = "Click a link above to begin diving into AI slop.<br>Just remember, all the content here is AI generated and almost guaranteed to be incorrect.<br>Also loading times will be long, please be patient."
+        title = "Welcome to AI Slop!"
+        article = "Click a link above to begin diving into AI slop.<br>Just remember, all the content here is AI generated and almost guaranteed to be incorrect.<br>Also loading times will be long, please be patient.<br><br>Above you will find 4 randomly generated words selected by an LLM, clicking on one of them will prompt the LLM to write a short essay and list a few fun facts about the selected word."
 
     else:
         title = request.args.get('article')
-
-        body["prompt"] = f"write a very short essay about {title} and list a few interesting facts about it."
-        article = requests.post("http://192.168.1.100:11434/api/generate", json=body)
-        article = json.loads(article.content)
+        if " " in title:
+            title = "Nice try"
+            article = "you're welcome to do that just keep it to one word.<br>Something something input rejection."
+        else:
+            body = {
+                "model": "llama3.2",
+                "prompt": f"write a short essay about {title} and list a few interesting facts about it.",
+                "stream": False
+            }
+            article = requests.post("http://192.168.1.100:11434/api/generate", json=body)
+            article = json.loads(article.content)
+            article = markdown(article['response'])
    
-    return render_template('/index.html', title=title, article=markdown(article['response']), links=links)
+    return render_template('/index.html', title=title, article=article, links=links)
 
 @app.route("/why", methods = ["GET"])
 def why():
--- dockerfile
+++ dockerfile
@@ -10,4 +10,5 @@ COPY . .
 
 EXPOSE 9080
 
-CMD ["python", "app.py"]
+#CMD ["python", "app.py"]
+CMD ["gunicorn", "-w", "4", "-b", "0.0.0.0:9080", "-t", "360", "app:app"]
