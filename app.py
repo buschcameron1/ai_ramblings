877436895edb9f96a128007224a35343bdc02107
Cameron <buschcameron1@gmail.com>
2025-01-10 11:31:59 -0600

    Adding comments, updating server IP

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request
-import json, requests
+import json, requests, random
 from markdown import markdown
 
 app = Flask(__name__)
@@ -8,6 +8,8 @@ app = Flask(__name__)
 def main():
     links = requests.get("https://random-word-api.herokuapp.com/word?number=4")
     links = json.loads(links.content)
+    comments = {}
+    usernames = {}
         
     if not request.args.get('article'):
         title = "Welcome to AI Slop!"
@@ -24,11 +26,27 @@ def main():
                 "prompt": f"write a short essay about {title} and list a few interesting facts about it.",
                 "stream": False
             }
-            article = requests.post("http://192.168.1.100:11434/api/generate", json=body)
+            article = requests.post("http://192.168.1.103:11434/api/generate", json=body)
             article = json.loads(article.content)
             article = markdown(article['response'])
+
+            commentNum = random.randrange(1, 15)
+            body["prompt"] = f"generate {commentNum} random innternet usernames. Return only usernames, comma separated."
+            usernames = requests.post("http://192.168.1.103:11434/api/generate", json=body)
+            usernames = json.loads(usernames.content)
+            usernames = usernames['response'].split(", ")
+
+            body["prompt"] = f"{commentNum} ragebait incorrect comment about the following article. Return only comments, comma seperated. article: {article}"
+
+            comments = requests.post("http://http://192.168.1.103:11434/api/generate", json=body)
+            comments = json.loads(comments.content)
+            comments = comments['response'].split(", ")
+
+            comments = dict(zip(usernames, comments))
+
+
    
-    return render_template('/index.html', title=title, article=article, links=links)
+    return render_template('/index.html', title=title, article=article, links=links, comments=comments)
 
 @app.route("/why", methods = ["GET"])
 def why():
